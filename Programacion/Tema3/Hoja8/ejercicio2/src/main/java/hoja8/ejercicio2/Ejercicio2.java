176d882039185688910bf654f827fd0c8a6a95eb
Fran898 <xsitjardecos@gmail.com>
2023-12-11 23:34:09 +0100

    update

--- Programacion/Tema3/Hoja8/ejercicio2/src/main/java/hoja8/ejercicio2/Ejercicio2.java
+++ Programacion/Tema3/Hoja8/ejercicio2/src/main/java/hoja8/ejercicio2/Ejercicio2.java
@@ -15,26 +15,43 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         Random random = new Random();
-        
-        
-        int num = 0;
+        double num = 0;
+        double total = 0;
+        int numbola;
+        int des = 0;
 
         while (num >= 0) {
             System.out.println("Introduce total a pagar, -1 para salir:");
             num = teclado.nextInt();
-            int numbola = random.nextInt(0, 4);
+            numbola = random.nextInt(0, 4);
             String color;
-            switch(numbola){
-                case 0 -> color = "blanco";
-                case 1 -> color = "amarillo";
-                case 2 -> color = "azul";
-                case 3 -> color = "rojo";
-                default -> color = "";
-                
+            switch (numbola) {
+                case 0 ->
+                    color = "blanco";
+                case 1 -> {
+                    color = "amarillo";
+                    des = 5;
+                }
+                case 2 -> {
+                    color = "azul";
+                    des = 7;
+                }
+                case 3 -> {
+                    color = "rojo";
+                    des = 10;
+                }
+                default ->
+                    color = "";
+
             }
-            
-            
-        
+            if (num >= 0) {
+                if (des > 1) {
+                    total = ((num * des) / 100);
+                }
+                System.out.printf("Ha salido la bola %s, %d por ciento de descuento: %,.2f Importe con el descuento: %,.2f \n ", color, des, total, (num - total));
+
+            }
+
         }
     }
 
