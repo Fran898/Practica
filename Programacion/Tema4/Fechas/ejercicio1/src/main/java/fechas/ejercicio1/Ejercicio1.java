85ee0c723c517dcde49d405450559e921510e702
Fran898 <xsitjardecos@gmail.com>
2024-01-10 17:28:54 +0100

    update

--- Programacion/Tema4/Fechas/ejercicio1/src/main/java/fechas/ejercicio1/Ejercicio1.java
+++ Programacion/Tema4/Fechas/ejercicio1/src/main/java/fechas/ejercicio1/Ejercicio1.java
@@ -11,6 +11,25 @@ package fechas.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+        Letra letra[] = new Letra[3];
         
+        for(int i = 0; i < letra.length; i++){
+            System.out.println("------- Letra Nº : " + (i+1) + " -------" );
+            letra[i] = new Letra();
+        }
+        
+        for(int i = 0; i < letra.length; i++){
+            if(!letra[i].vencida()){
+                letra[i].mostrar();
+            }
+            if(letra[i].devuelveMes().equals("enero")){
+                letra[i].demoran(15);
+            }
+        }
+        for(int i = 0; i < letra.length; i++){
+            letra[i].getTitular();
+            letra[i].diasFaltan();
+        }
+       
     }
 }
--- Programacion/Tema4/Fechas/ejercicio1/src/main/java/fechas/ejercicio1/Letra.java
+++ Programacion/Tema4/Fechas/ejercicio1/src/main/java/fechas/ejercicio1/Letra.java
@@ -4,8 +4,12 @@
  */
 package fechas.ejercicio1;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -13,30 +17,58 @@ import java.util.Scanner;
  * @author DAM114
  */
 public class Letra {
+
     private int numLetra;
     private String titular;
     private double importe;
     private LocalDate fechaVencimiento;
-    
-    
-    
-    public Letra(){
+
+    public Letra() {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Inserte el numero de letra: ");
         numLetra = teclado.nextInt();
         System.out.println("Inserte el nombre del titular: ");
-        titular = teclado.nextLine();
+        titular = teclado.next();
         System.out.println("Inserte el importe: ");
         importe = teclado.nextDouble();
         System.out.println("Inserte la fecha con formato dd-mm-yyyy: ");
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-mm-yyyy");
-        fechaVencimiento = LocalDate.parse(teclado.nextLine(),f);
-        
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        fechaVencimiento = LocalDate.parse(teclado.next(), f);
+
+    }
+
+    public boolean vencida() {
+        return fechaVencimiento.compareTo(LocalDate.now()) < 0;
+
+    }
+
+    public void demoran(int n) {
+        fechaVencimiento = fechaVencimiento.plusDays(n);
+
+    }
+
+    public long diasFaltan() {
+        return Duration.between(LocalDate.now().atStartOfDay(), fechaVencimiento.atStartOfDay()).toDays();
         
     }
     
+    public void mostrar(){
+        System.out.println("Titular: " + titular);
+        System.out.println("Numero de letra: " + numLetra);
+        System.out.println("Importe: " + importe);
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        System.out.println("Fecha de vencimiento: " + fechaVencimiento.format(f));
+        
+    }
     
+    public String devuelveMes(){
+        
+        return fechaVencimiento.getMonth().getDisplayName(TextStyle.FULL, Locale.getDefault()); 
+    }
+
+    public String getTitular() {
+        return titular;
+    }
     
-    
-    
+
 }
