ef5a6be545d8d1e77da2111d59e494879f4cf144
Fran898 <xsitjardecos@gmail.com>
2024-01-16 19:57:31 +0100

    update

---
+++ Entorno de Desarrollo/Programas/Temporal/src/main/java/main/temporal/Temporal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.temporal;
+
+/**
+ *
+ * @author DAM114
+ */
+public class Temporal {
+
+    public static void main(String[] args) {
+        int temp = Integer.MAX_VALUE - (-1);
+        System.out.println(temp);
+    }
+}
