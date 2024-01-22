76467ce534ff475ece481b8cf205e573b391d991
Fran898 <xsitjardecos@gmail.com>
2024-01-22 19:07:30 +0100

    update

--- Programacion/Tema4/Hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Ejercicio3.java
+++ Programacion/Tema4/Hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Ejercicio3.java
@@ -4,6 +4,8 @@
 
 package hoja8.ejercicio3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Francisco Sitjar
@@ -11,6 +13,30 @@ package hoja8.ejercicio3;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Pais pais = new Pais("España");
         
+        int opc;
+        do {
+            System.out.println("Selecciona un opcion: ");
+            System.out.println("1- Añadir ciudad");
+            System.out.println("2- Ciudad con mas habitantes");
+            System.out.println("3- Existe la ciudad o no?");
+            System.out.println("4- Media de habitantes en todas las ciudades");
+            System.out.println("5- Datos de todas las ciudades");
+            System.out.println("6- Salir");
+            opc = teclado.nextInt();
+            switch (opc) {
+                case 1 -> pais.addCiudad();
+                case 2 -> System.out.println(pais.masHab());
+                case 3 -> System.out.println("El pais existe: " + pais.existe());
+                case 4 -> System.out.println("La media de los habitantes en todas las ciudades es: " + pais.mediaTotal());
+                case 5 -> System.out.println(pais.toString());
+                case 6 -> opc = 6;
+                default ->
+                    System.out.println("Opcion no valida");                
+            }
+            
+        } while (opc != 6);
     }
 }
--- Programacion/Tema4/Hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Pais.java
+++ Programacion/Tema4/Hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Pais.java
@@ -19,7 +19,7 @@ public class Pais {
         
     
     public Pais(String nombre){
-        ciudad = new Ciudad[25];
+        ciudad = new Ciudad[2];
         nciud = 0;
     }
     
@@ -43,15 +43,46 @@ public class Pais {
     
     
     public String masHab(){
-        /*Arrays.sort(ciudad,Comparator.comparing(Ciudad::getHab));
-        return "La ciudad con mas habitantes es: " + (ciudad.length-1);*/
-        String temp = ciudad[0].getNombre();
+        Arrays.sort(ciudad,Comparator.comparing(Ciudad::getHab));
+        return "La ciudad con mas habitantes es: " + ciudad[(ciudad.length-1)].getNombre();
+       /* String temp = ciudad[0].getNombre();
         for(int i = 1; i < ciudad.length; i++){
             if(ciudad[i].getHab() >= ciudad[i-1].getHab()){
                 temp = ciudad[i].getNombre();
             }
         }
+        return temp;*/
+    }
+    
+    public boolean existe(){
+        Scanner teclado = new Scanner(System.in);
+        boolean temp = false;
+        System.out.println("Inserte el nombre de la ciudad: ");
+        String texto = teclado.nextLine();
+        for (int i = 0; i < ciudad.length; i++) {
+            if(texto.equalsIgnoreCase(ciudad[i].getNombre())){
+                temp = true;
+            }
+        }
         return temp;
+    }
+    
+    public int mediaTotal(){
+        int suma = 0;
+        for (Ciudad temp : ciudad) {
+            suma += temp.getHab();
+        }
+        return suma/ciudad.length;
+    }
+    
+    @Override
+    public String toString(){
+        String texto = "";
+        for(Ciudad temp : ciudad){
+            texto += "Ciudad: " + temp.getNombre() + "\nHabitantes: " + temp.getHab() + "\nLongitud: " + temp.getLon() +
+            "\nLatitud: " + temp.getLat() + "\n-----------------------------------------\n";
+        }
+        return texto;
     }
     
 }
