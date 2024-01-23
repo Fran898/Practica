70f731e05cebf647c4310dcbc034193c502ae2ba
Fran898 <xsitjardecos@gmail.com>
2024-01-23 15:02:55 +0100

    update

--- Programacion/Tema4/Main/ejercicio1/src/main/java/main/ejercicio1/Ejercicio1.java
+++ Programacion/Tema4/Main/ejercicio1/src/main/java/main/ejercicio1/Ejercicio1.java
@@ -10,14 +10,18 @@ package main.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        double potencia = args[0].length();
-        double num1 = Integer.parseInt(args[0].substring(0, 1));
-        double num2 = Integer.parseInt(args[0].substring(1, 2));
-        double num3 = Integer.parseInt(args[0].substring(2, 3));
-        if (((Math.pow(num1, potencia)) + (Math.pow(num2, potencia)) + (Math.pow(num3, potencia))) == Integer.parseInt(args[0])) {
-            System.out.println("Es Armstrong");
+        if (args[0].length() > 0 && args[0].length() < 4) {
+            int potencia = args[0].length();
+            int num1 = Integer.parseInt(args[0].substring(0, 1));
+            int num2 = Integer.parseInt(args[0].substring(1, 2));
+            int num3 = Integer.parseInt(args[0].substring(2, 3));
+            if (((Math.pow(num1, potencia)) + (Math.pow(num2, potencia)) + (Math.pow(num3, potencia))) == Integer.parseInt(args[0])) {
+                System.out.println("Es Armstrong");
+            } else {
+                System.out.println("No es Armstrong");
+            }
         }else{
-            System.out.println("No es Armstrong");
+            System.out.println("No es un numero valido");
         }
 
     }
