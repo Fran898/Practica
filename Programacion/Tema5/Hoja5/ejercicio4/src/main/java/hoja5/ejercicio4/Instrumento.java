135d228a0f8bc4e29f2c051b877be36c14600453
Fran898 <xsitjardecos@gmail.com>
2024-02-22 16:26:05 +0100

    update

---
+++ Programacion/Tema5/Hoja5/ejercicio4/src/main/java/hoja5/ejercicio4/Instrumento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja5.ejercicio4;
+
+/**
+ *
+ * @author Francisco Sitjar
+ */
+public abstract class Instrumento {
+    
+    public abstract String tocarNota();
+    
+    public 
+    
+}
---
+++ Programacion/Tema5/Hoja7/ejercicio1/src/main/java/hoja7/ejercicio1/Ejercicio1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja7.ejercicio1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Francisco Sitjar
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        do{
+            try{
+                System.out.println("Inserte un numero entero: ");
+                int entero = new Scanner(System.in).nextInt();
+                System.out.println("Inserte un numero con decimales: ");
+                double dec = new Scanner(System.in).nextDouble();
+                
+                
+            }catch (InstantiationException e){
+                
+            }
+            
+            
+        }
+        
+        
+        
+        
+    }
+}
