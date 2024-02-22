a3d7bad88b25c4fe545a215f5a7ac590893199db
Fran898 <xsitjardecos@gmail.com>
2024-02-22 15:18:14 +0100

    update

---
+++ Programacion/Tema5/Hoja5/ejercicio4/src/main/java/hoja5/ejercicio4/Ejercicio4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja5.ejercicio4;
+
+/**
+ *
+ * @author Francisco Sitjar
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
