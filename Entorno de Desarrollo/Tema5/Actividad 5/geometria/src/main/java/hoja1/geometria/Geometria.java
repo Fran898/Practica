d23a909134a1208d7fd6f69f80a3a29c542a45dc
Fran898 <xsitjardecos@gmail.com>
2024-03-22 18:49:35 +0100

    update

---
+++ Entorno de Desarrollo/Tema5/Actividad 5/geometria/src/main/java/hoja1/geometria/Geometria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hoja1.geometria;
+
+/**
+ *
+ * @author DAM114
+ */
+public class Geometria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+    }
+}
